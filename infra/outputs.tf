8d17e2ed3e337c4f37f79ab9430877e044e92f22
Michael Popovich <earthmabus@hotmail.com>
2026-07-03 22:03:02 -0400

    removed confirm account

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -63,10 +63,6 @@ output "cognito_user_pool_client_id" {
   description = "Cognito User Pool Web Client ID"
 }
 
-output "cloudfront_distribution_id" {
-  value = aws_cloudfront_distribution.frontend.id
-}
-
 output "cloudfront_domain_name" {
   value = aws_cloudfront_distribution.frontend.domain_name
 }
