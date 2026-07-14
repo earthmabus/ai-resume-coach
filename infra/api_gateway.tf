0a22949ab79ee3ececad37062c0f65d0e3ed7ecf
Michael Popovich <earthmabus@hotmail.com>
2026-07-14 19:05:50 -0400

    dr - regional runtime foundation

--- infra/api_gateway.tf
+++ infra/api_gateway.tf
@@ -34,18 +34,6 @@ resource "aws_apigatewayv2_route" "health_ready_route" {
   target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
 }
 
-resource "aws_apigatewayv2_route" "health_live_route" {
-  api_id    = aws_apigatewayv2_api.api.id
-  route_key = "GET /health/live"
-  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
-}
-
-resource "aws_apigatewayv2_route" "health_ready_route" {
-  api_id    = aws_apigatewayv2_api.api.id
-  route_key = "GET /health/ready"
-  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
-}
-
 resource "aws_apigatewayv2_route" "version_route" {
   api_id    = aws_apigatewayv2_api.http_api.id
   route_key = "GET /version"
